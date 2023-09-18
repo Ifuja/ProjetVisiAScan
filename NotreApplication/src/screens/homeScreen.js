617f2963495d44c21461f93082fb17f8d2ddcf33
Ifuja CHAMOINRI <ifuja.chamoinri@gmail.com>
2023-09-18 16:33:05 +0200

    Ajustement du code source

--- NotreApplication/src/screens/homeScreen.js
+++ NotreApplication/src/screens/homeScreen.js
@@ -1,23 +1,73 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View, Image, BackHandler } from 'react-native';
 import { Title } from 'react-native-paper';
 
 import FormButton from '../components/formButton';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
+  const handleLogout = () => {
+    BackHandler.exitApp(); // Cette ligne ferme l'application lorsque le bouton "Logout" est appuyé
+  };
   return (
     <View style={styles.container}>
-      <Title>VisiAScan</Title>
-      <FormButton modeValue='contained' title='Logout' />
+      <ImageBackground
+        source={require('../../assets/ia.gif')} // Remplacez par le chemin de votre fichier GIF
+        style={styles.backgroundImage}>
+        <View style={styles.buttonContainer}>
+          <Image
+            source={require('../../assets/logo_app.png')}
+            style={styles.media}
+          />
+          <Title style={styles.titleText}>Welcome to VisiaScan!</Title>
+          <FormButton 
+            modeValue='contained' 
+            title='Access the application' 
+            labelStyle={styles.loginButtonLabel}
+            buttonColor='#008080'
+            onPress={() => {
+              navigation.navigate('Login');
+            }}
+          />
+          <FormButton 
+            modeValue='contained' 
+            title='Exit the application'
+            labelStyle={styles.loginButtonLabel}
+            buttonColor='#CC6666'
+            onPress={handleLogout}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#f5f5f5',
+    flex: 1
+  },
+  loginButtonLabel: {
+    fontSize: 19,
+    color:'white'
+  },
+  titleText: {
+    fontSize: 24,
+    marginBottom: 10,
+    color:'white'
+  },
+  media: {
+    width: '60%',
+    height: '40%',
+    borderRadius: 150,
+    marginBottom: 100
+  },  
+  backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    resizeMode: 'cover', // Vous pouvez ajuster le mode de redimensionnement selon vos besoins
+    justifyContent: 'center', // Vous pouvez ajuster l'alignement vertical selon vos besoins
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center', // Centre les boutons horizontalement
+    alignItems: 'center', // Centre les boutons verticalement
   }
 });
--- NotreApplication/src/screens/loginScreen.js
+++ NotreApplication/src/screens/loginScreen.js
@@ -1,20 +1,32 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
-import { Title } from 'react-native-paper';
+import { Alert, Image, ImageBackground, StyleSheet, View } from 'react-native';
+import { IconButton, Title } from 'react-native-paper';
 import { AuthContext } from '../context/authProvider';
 import { auth } from '../firebase';
+import messaging from '@react-native-firebase/messaging';
 import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
 
 import FormButton from '../components/formButton.js';
 import FormInput from '../components/formInput.js';
-import SocialButton from '../components/socialButton.js';
+//import SocialButton from '../components/socialButton.js';
 
 export default function LoginScreen({ navigation }) {
   const [ email, setEmail ] = useState('');
   const [ user, setUser ] = useState();
   const [ password, setPassword ] = useState('');
-  const { login, loginWithGoogle, logout } = useContext(AuthContext);
+  const { login, loginWithGoogle, logout, logoutWithGoogle } = useContext(AuthContext);
   const [ initializing, setInitializing ] = useState(true);
+  
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled = 
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
 
   /*** Connexion avec un compte Google ***/
   const handleGoogleLogin = async () => {
@@ -37,6 +49,7 @@ export default function LoginScreen({ navigation }) {
       // Une fois que la connexion est réussie et l'utilisateur est redirigé
       if (loggedInUser) {
         console.log('Etat de la connexion:', loggedInUser);
+        sendNotification('Bienvenue !', 'Vous êtes connecté avec succès.');
       }
     } catch (error) {
       // Gére les erreurs de connexion ici
@@ -51,7 +64,56 @@ export default function LoginScreen({ navigation }) {
 
   useEffect(() => {
     const subscriber = auth.onAuthStateChanged(onAuthStateChanged);
-    return subscriber;
+
+    if (requestUserPermission()){
+      // return fcm token from the device
+      messaging().getToken().then(token => {
+          console.log(token);
+      });
+    }
+    else {
+      console.log("Failed token status", authStatus);
+    }
+
+    //Check weather an initial notification is available
+    messaging()
+      .getInitialNotification()
+      .then( async (remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+          // Naviguer vers la vue "Porte" 
+          //navigation.navigate('Chat');
+        }
+    });
+
+    // Assume a message-notification contains a "type" property in the data payload
+    messaging().onNotificationOpenedApp( async (remoteMessage) => {
+      navigation.navigate('Chat');
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );
+    });
+
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    // Alert message arrived on my app
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return () => {
+      // Vous pouvez ajouter d'autres nettoyages ici si nécessaire.
+      subscriber(); // Assurez-vous d'appeler la fonction renvoyée par `onAuthStateChanged`.
+      unsubscribe(); // Assurez-vous d'appeler la fonction renvoyée par `onMessage`.
+    };
+
   }, []);
 
   if (initializing) return null;
@@ -63,7 +125,10 @@ export default function LoginScreen({ navigation }) {
           source={require('../../assets/ia.gif')} // Remplacez par le chemin de votre fichier GIF
           style={styles.backgroundImage}>
           <View style={styles.buttonContainer}>
-            <Title style={styles.titleText}>Welcome to VisiaScan!</Title>
+            <Image
+              source={require('../../assets/logo_app.png')}
+              style={styles.media}
+            />
             <FormInput
               labelName='Email'
               value={email}
@@ -77,25 +142,11 @@ export default function LoginScreen({ navigation }) {
               onChangeText={(userPassword) => setPassword(userPassword)}
             />
             <FormButton
-              title='Login'
+              title='Sign in'
               modeValue='contained'
               labelStyle={styles.loginButtonLabel}
-              buttonColor='#F5D025'
+              buttonColor='#5CA9CB'
               onPress={handleLogin} // Appel la fonction handleLogin lors du clic sur le bouton
-            />
-            <SocialButton 
-              buttonTitle="Sign Up with Facebook"
-              btnType="facebook"
-              color="#4867aa"
-              backgroundColor="#e6eaf4"
-              onPress={handleLogin}
-            />
-            <SocialButton 
-              buttonTitle="Sign Up with Google"
-              btnType="google"
-              color="#de4d41"
-              backgroundColor="#f5e7ea"
-              onPress={handleGoogleLogin}
             />
             <GoogleSigninButton
               size={GoogleSigninButton.Size.Wide}
@@ -110,6 +161,13 @@ export default function LoginScreen({ navigation }) {
               labelStyle={styles.navButtonText}
               onPress={() => 
                 navigation.navigate('Signup')}
+            />  
+            <IconButton
+              icon='keyboard-backspace'
+              size={30}
+              style={styles.navButton}
+              iconColor='white'
+              onPress={() => navigation.goBack()}
             />
           </View>
         </ImageBackground>
@@ -122,40 +180,49 @@ export default function LoginScreen({ navigation }) {
         source={require('../../assets/ia.gif')} // Remplacez par le chemin de votre fichier GIF
         style={styles.backgroundImage}>
         <View style={styles.buttonContainer}>       
+          <View style={{ flex: 1 }} />
+          <Image
+            source={{uri: user.photoURL }}
+            style={styles.media}
+          />
           <Title style={styles.titleText}>VisiaScan Menu</Title>
           <FormButton
-              title='Users'
-              modeValue='contained'
-              labelStyle={styles.loginButtonLabel}
-              buttonColor='#F5D025'
-              onPress={() => {
-              navigation.navigate('Users');
-              }} 
+            title='Users'
+            modeValue='contained'
+            labelStyle={styles.loginButtonLabel}
+            buttonColor='#5CA9CB'
+            onPress={() => {
+            navigation.navigate('Users');
+            }} 
           />
           <FormButton
-              title='Call'
-              modeValue='contained'
-              labelStyle={styles.loginButtonLabel}
-              buttonColor='#F5D025'
-              onPress={() => {
-              navigation.navigate('Call');
-              }}
+            title='Pictures'
+            modeValue='contained'
+            labelStyle={styles.loginButtonLabel}
+            buttonColor='#5CA9CB'
+            onPress={() => {
+            navigation.navigate('Call');
+            }}
           />
           <FormButton
-              title='Notification'
-              modeValue='contained'
-              labelStyle={styles.loginButtonLabel}
-              buttonColor='#F5D025'
-              onPress={() => {
-              navigation.navigate('Notification');
-              }}
+            title='Token'
+            modeValue='contained'
+            labelStyle={styles.loginButtonLabel}
+            buttonColor='#5CA9CB'
+            onPress={() => {
+            navigation.navigate('Notification');
+            }}
           />
+          <View style={{ flex: 1 }} />
           <FormButton
-              title='Sign Out'
-              labelStyle={styles.navButtonText}
-              uppercase={false}
-              modeValue='text'
-              onPress={() => logout()}
+            title='Sign Out'
+            labelStyle={styles.navButtonText}
+            modeValue='contained'
+            buttonColor='#CC6666'
+            onPress={() => {
+              logoutWithGoogle();
+              logout();
+            }}
           />
         </View> 
       </ImageBackground>
@@ -190,4 +257,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center', // Centre les boutons horizontalement
     alignItems: 'center', // Centre les boutons verticalement
   },
+  media: {
+    width: '30%',
+    height: '20%',
+    borderRadius: 150,
+    marginBottom: 40
+  }
 });
